8d223fc829560e7ddf9cfbb09d7e1597c03b794d
TobiasVeronaLaser <tobias.laser@ymail.com>
2024-06-15 16:25:57 +0200

    well still dono

--- src/main.cpp
+++ src/main.cpp
@@ -1,98 +1,118 @@
+/** Rock-Paper-Scissors Console-Version Prototype-Version
+ * Author: Tobias Laser
+*/
+
 #include <iostream>
 #include <cstdlib>
 #include <string>
 
 using namespace std;
 
-enum Hand {ROCK, PAPER, SCISSORS};
-enum GameMode {SINGLEPLAYER, MULTIPLAYER};
+enum Hand {UNDEFINED, ROCK, PAPER, SCISSORS};
 
-class Player{
-    private:
-        string name;
-        Hand hand;
-    
-    public:
-
-    // Constructor
-    
-    Player(){
-        setName("COM");
-        setHand(undefined);
-    }
-
-    Player(string name){
-        setName(name);
-        setHand((Hand)UNDEFINED);
-    }
-
-    // Setter
-
-    void setName(string name){
-        this->name = name;
-    }
-
-    void setHand(Hand hand){
-        this->hand = hand;
-    }
-
-    // Getter
-
-    string getName(){
-        return name;
-    }
-
-    Hand getHand(){
-        return hand;
-    }
-
-};
-
-class Game{
-    private:
-        static const int playerCount = 2;
-        static Player players[playerCount];
-        static Game *instance;
-        Game(){}
-
-    public:
-        Game *getInstance(){
-            if(!instance){
-                this->instance = new Game();
-            }
-            return this->instance;
-        }
-
-    bool checkBoundaries(int index){
-        return 0 <= index && index < playerCount;
-    }
-
-    bool setName(int index, string name){
-        if(checkBoundaries(index)){
-            players[index].setName(name);
-        }
-    }
-
-    bool setHand(int index, Hand hand){
-        if(checkBoundaries(index)){
-            players[index].setHand(hand);
-        }
-    }
-
-    int checkWinner(){
-        Hand h1 = players[0].getHand(), h2 = players[1].getHand();
-        if(h1 == ()UNDEFINED || h2 == (Hand)UNDEFINED){
-
-        }
-        if(h1 == h2){
-            return 2;
-        }
-
+/**
+ * returns ...
+ *  ... 2:  p1 win
+ *  ... 1:  draw
+ *  ... 0:  p1 lose
+ *  ... -1: Unexpected input
+*/
+int check(Hand p1, Hand p2){
+    if(p1 == UNDEFINED || p2 == UNDEFINED){
         return -1;
     }
+    if(p1 == p2){
+        return 1;
+    }
+    if(p1 + 1 % 3 == p2){
+        return 0;
+    }
+    if(p1 == p2 + 1 % 3){
+        return 2;
+    }
+    return -1;
 
-};
+}
+
+Hand getHand(int value){
+    switch(value){
+        case 1: 
+            return ROCK;
+        case 2:
+            return PAPER;
+        case 3:
+            return SCISSORS;
+        case 0:
+        default: 
+            return UNDEFINED;
+    }
+}
+
+string getName(Hand value){
+    switch(value){
+        case ROCK:
+            return "rock";
+        case PAPER:
+            return "paper";
+        case SCISSORS:
+            return "scissors";
+        case UNDEFINED:
+        default:
+            return "undefined";
+    }
+}
 
 int main(void){
+    printf("Welcome to Rock-Paper-Scissors Console-Version Prototype-Version\n");
+    bool isPlaying = true;
+    do{
+        int gameMode = -1;
+        do{
+            printf("\nChoose your game mode or exit:\n0:\texit\n1:\tsingle-player\n2:\tmultiplayer\nYour pick:\t");
+
+        }while(1 != scanf("%d", &gameMode) || 0 > gameMode || gameMode > 2);
+            printf("\n");
+            switch(gameMode){
+                case 0: 
+                    isPlaying = false;
+                    printf("Till next time!\n");
+                    break;
+                case 1: 
+                    printf("Single-player!\n");
+                    do{
+                        int choice = -1;
+                        do{
+                            printf("Player choose your hand or exit:\n0:\texit\n1:\tRock\n2:\tPaper\n3:\tScissors\n");
+                        }while(1 != scanf("%d", &choice) || 0 > choice || choice > 3);
+                        if(0 == choice){
+                            break;
+                        }
+                        Hand p = getHand(choice), com = getHand(3 * 0.9999 * rand() / (RAND_MAX) + 1);
+                        printf("Player:\t%s\tCOM:\t%s\n", getName(p), getName(com));
+                        switch (check(p, com)){
+                            case 2: 
+                                printf("You Win!\n");
+                                break;
+                            case 1:
+                                printf("A draw.\n");
+                                break;
+                            case 0: 
+                                printf("You lose!\n");
+                                break;
+                            default:
+                                printf("Something unexpected happened!\n");
+                                return EXIT_FAILURE;
+                        }
+                    }while(true);
+                    break;
+                case 2:
+                    printf("Multiplayer!\n");
+                    break;
+                default: 
+                    isPlaying = false;
+                    printf("Something unexpected happened!\n");
+                    return EXIT_FAILURE;
+        }
+    }while(isPlaying);
     return EXIT_SUCCESS;
 }
